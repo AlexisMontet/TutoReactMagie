9a3eab1bca1399b3c2ab139fc866025a9d57e160
Raptoroftanks <alexis.montet@orange.fr>
2023-12-22 13:22:34 +0100

    Push du projet part2

---
+++ Defi 21102023/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from "gatsby"
+import Header from "../components/header"
+import Body from "../components/body"
+
+const Index= () =>{
+  return(
+    <div>
+      <Header></Header>
+
+    </div>
+  )
+}
+
+export default Index
